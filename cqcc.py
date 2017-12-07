4ddd2c4ca7bcf6846b891fe9521a27f23857b57c
azraelkuan <azraelkuan@gmail.com>
2017-12-07 20:38:02 +0800

    fix the model

---
+++ cqcc.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import librosa
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -18,6 +18,7 @@ def use_cuda():
 
 
 def get_test_accuracy(dataset, net):
+    net.eval()
     correct = 0
     for i, tmp in enumerate(dataset):
         data = Variable(tmp['data'])
@@ -40,23 +41,24 @@ def main():
     print(print_str.format("Loading Data"))
     train_data, train_label = load_data("train", "data/protocol/ASVspoof2017_train.trn.txt", mode="train")
     train_dataset = ASVDataSet(train_data, train_label, mode="train")
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, num_workers=1, shuffle=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, num_workers=2, shuffle=True)
 
     dev_data, dev_label, dev_wav_ids = load_data("dev", "data/protocol/ASVspoof2017_dev.trl.txt", mode="test")
     dev_dataset = ASVDataSet(dev_data, dev_label, wav_ids=dev_wav_ids, mode="test")
 
-    test_data, test_label, test_wav_ids = load_data("eval", "data/protocol/ASVspoof2017_eval_v2_key.trl.txt", mode="test")
-    test_dataset = ASVDataSet(test_data, test_label, wav_ids=test_wav_ids, mode="test")
+    # test_data, test_label, test_wav_ids = load_data("eval", "data/protocol/ASVspoof2017_eval_v2_key.trl.txt", mode="test")
+    # test_dataset = ASVDataSet(test_data, test_label, wav_ids=test_wav_ids, mode="test")
 
-    model = DNNModel(input_dim=351, hidden_dim=4096, output_dim=2)
+    model = DNNModel(input_dim=351, hidden_dim=2048, output_dim=2)
     if use_cuda():
         model = model.cuda()
     cross_entropy = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(params=model.parameters(), lr=0.01, weight_decay=5e-4)
+    optimizer = optim.Adam(params=model.parameters(), lr=0.01)
     scheduler = MultiStepLR(optimizer, milestones=[3, 7], gamma=0.1)
 
     for epoch in range(num_epochs):
         scheduler.step()
+        model.train()
         for i, tmp in enumerate(train_dataloader):
             data = Variable(tmp['data'])
             label = Variable(tmp['label'])
@@ -76,7 +78,8 @@ def main():
 
                 # to test the dev data and test data
                 dev_accuracy = get_test_accuracy(dev_dataset, model)
-                test_accuracy = get_test_accuracy(test_dataset, model)
+                test_accuracy = 0.0
+                # test_accuracy = get_test_accuracy(test_dataset, model)
                 print(" Dev Acc: %.2f Test Acc: %.2f" % (dev_accuracy, test_accuracy))
 
 
